bc20e97641d5a2ce3749b829950e61326d88906d
Mikko Salomäki <ms@datarespons.se>
2020-01-15 15:58:03 +0100

    nvram: add comment for usage

--- nvram.h
+++ nvram.h
@@ -7,8 +7,40 @@
 
 struct nvram;
 
+/*
+ * Initialize nvram and get list of variables
+ *
+ * @params
+ *   nvram: private data
+ *   list: returned list
+ *   section_a: String (i.e. path) for section A. The pointer must remain valid during program execution.
+ *   section_b: String (i.e. path) for section B. The pointer must remain valid during program execution.
+ *
+ * @returns
+ *   0 for success
+ *   negative errno for error
+ */
 int nvram_init(struct nvram** nvram, struct nvram_list* list, const char* section_a, const char* section_b);
+
+/*
+ * Commit list of variables to nvram
+ *
+ * @params
+ *   nvram: private data
+ *   list: list to commit
+ *
+ * @returns
+ *   0 for success
+ *   negative errno for error
+ */
 int nvram_commit(struct nvram* nvram, const struct nvram_list* list);
+
+/*
+ * Close nvram after usage
+ *
+ * @params
+ *   nvram: private data
+ */
 int nvram_close(struct nvram** nvram);
 
 #endif // _NVRAM_H_
