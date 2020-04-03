b58ec72b30048a8b90c4303aa9c370be46f65808
Mikko Salomäki <ms@datarespons.se>
2020-04-03 10:51:30 +0200

    main.c: rename factory to system, add --sys argument

--- main.c
+++ main.c
@@ -15,11 +15,13 @@
 
 #define NVRAM_LOCKFILE "/run/lock/nvram.lock"
 #define NVRAM_ENV_DEBUG "NVRAM_DEBUG"
-#define NVRAM_ENV_FACTORY_MODE "NVRAM_FACTORY_MODE"
 #define NVRAM_ENV_USER_A "NVRAM_USER_A"
 #define NVRAM_ENV_USER_B "NVRAM_USER_B"
-#define NVRAM_ENV_FACTORY_A "NVRAM_FACTORY_A"
-#define NVRAM_ENV_FACTORY_B "NVRAM_FACTORY_B"
+#define NVRAM_ENV_SYSTEM_A "NVRAM_SYSTEM_A"
+#define NVRAM_ENV_SYSTEM_B "NVRAM_SYSTEM_B"
+#define NVRAM_ENV_SYSTEM_UNLOCK "NVRAM_SYSTEM_UNLOCK"
+#define NVRAM_SYSTEM_UNLOCK_MAGIC "16440"
+#define NVRAM_SYSTEM_PREFIX "SYS_"
 
 static int FDLOCK = 0;
 
@@ -39,6 +41,18 @@ static long get_env_long(const char* env)
 	if (val) {
 		char *endptr = NULL;
 		return strtol(val, &endptr, 10);
+	}
+	return 0;
+}
+
+static int starts_with(const char* str, const char* prefix)
+{
+	size_t str_len = strlen(str);
+	size_t prefix_len = strlen(prefix);
+	if (str_len > prefix_len) {
+		if(!strncmp(str, prefix, prefix_len)) {
+			return 1;
+		}
 	}
 	return 0;
 }
@@ -97,7 +111,7 @@ enum op {
 };
 
 struct opts {
-	int factory_mode;
+	int system_mode;
 	enum op op;
 	char* key;
 	char* val;
@@ -116,9 +130,6 @@ int main(int argc, char** argv)
 
 	if (get_env_long(NVRAM_ENV_DEBUG)) {
 		enable_debug();
-	}
-	if (get_env_long(NVRAM_ENV_FACTORY_MODE)) {
-		opts.factory_mode=1;
 	}
 
 	for (int i = 0; i < argc; i++) {
@@ -157,27 +168,65 @@ int main(int argc, char** argv)
 			opts.key = argv[i];
 			break;
 		}
+		else
+		if (!strcmp("--sys", argv[i])) {
+			opts.system_mode = 1;
+		}
 	}
 
-	struct nvram *nvram_factory = NULL;
-	struct nvram_list list_factory = NVRAM_LIST_INITIALIZER;
+	pr_dbg("operation: %d, key: %s, val: %s, system_mode: %d\n", opts.op, opts.key, opts.val, opts.system_mode);
+
+	if (opts.system_mode) {
+		switch (opts.op) {
+		case OP_SET:
+			if (!starts_with(opts.key, NVRAM_SYSTEM_PREFIX)) {
+				pr_err("required prefix \"%s\" missing in system attribute\n", NVRAM_SYSTEM_PREFIX);
+				return EINVAL;
+			}
+			//FALLTHROUGH
+		case OP_DEL:
+			if (strcmp(getenv(NVRAM_ENV_SYSTEM_UNLOCK), NVRAM_SYSTEM_UNLOCK_MAGIC)) {
+				pr_err("system write locked\n")
+				return EACCES;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	else {
+		if (opts.op == OP_SET) {
+			if (starts_with(opts.key, NVRAM_SYSTEM_PREFIX)) {
+				pr_err("forbidden prefix \"%s\" in user attribute\n", NVRAM_SYSTEM_PREFIX);
+				return EINVAL;
+			}
+		}
+	}
+
+	struct nvram *nvram_system = NULL;
+	struct nvram_list list_system = NVRAM_LIST_INITIALIZER;
 	struct nvram *nvram_user = NULL;
 	struct nvram_list list_user = NVRAM_LIST_INITIALIZER;
-	const char *nvram_factory_a = get_env_str(NVRAM_ENV_FACTORY_A, xstr(NVRAM_FACTORY_A));
-	const char *nvram_factory_b = get_env_str(NVRAM_ENV_FACTORY_B, xstr(NVRAM_FACTORY_B));
-	const char *nvram_user_a = get_env_str(NVRAM_ENV_USER_A, xstr(NVRAM_USER_A));
-	const char *nvram_user_b = get_env_str(NVRAM_ENV_USER_B, xstr(NVRAM_USER_B));
 
 	r = acquire_lockfile(NVRAM_LOCKFILE, &FDLOCK);
 	if (r) {
 		goto exit;
 	}
 
-	r = nvram_init(&nvram_factory, &list_factory, nvram_factory_a, nvram_factory_b);
+	const char *nvram_system_a = get_env_str(NVRAM_ENV_SYSTEM_A, xstr(NVRAM_SYSTEM_A));
+	const char *nvram_system_b = get_env_str(NVRAM_ENV_SYSTEM_B, xstr(NVRAM_SYSTEM_B));
+	pr_dbg("NVRAM_SYSTEM_A: %s\n", nvram_system_a);
+	pr_dbg("NVRAM_SYSTEM_B: %s\n", nvram_system_b);
+
+	r = nvram_init(&nvram_system, &list_system, nvram_system_a, nvram_system_b);
 	if (r) {
 		goto exit;
 	}
-	if (!opts.factory_mode) {
+	if (!opts.system_mode) {
+		const char *nvram_user_a = get_env_str(NVRAM_ENV_USER_A, xstr(NVRAM_USER_A));
+		const char *nvram_user_b = get_env_str(NVRAM_ENV_USER_B, xstr(NVRAM_USER_B));
+		pr_dbg("NVRAM_USER_A: %s\n", nvram_user_a);
+		pr_dbg("NVRAM_USER_B: %s\n", nvram_user_b);
 		r = nvram_init(&nvram_user, &list_user, nvram_user_a, nvram_user_b);
 		if (r) {
 			goto exit;
@@ -186,13 +235,14 @@ int main(int argc, char** argv)
 
 	switch (opts.op) {
 	case OP_LIST:
-		pr_dbg("listing all\n");
-		struct nvram_node* cur = list_factory.entry;
+		pr_dbg("listing system\n");
+		struct nvram_node* cur = list_system.entry;
 		while(cur) {
 			printf("%s=%s\n", cur->key, cur->value);
 			cur = cur->next;
 		}
-		if(!opts.factory_mode) {
+		if(!opts.system_mode) {
+			pr_dbg("listing user\n");
 			cur = list_user.entry;
 			while(cur) {
 				printf("%s=%s\n", cur->key, cur->value);
@@ -201,44 +251,39 @@ int main(int argc, char** argv)
 		}
 		break;
 	case OP_GET:
-		pr_dbg("getting: %s\n", opts.key);
-		opts.val = nvram_list_get(&list_factory, opts.key);
-		if (!opts.val) {
+		pr_dbg("getting key from system: %s\n", opts.key);
+		opts.val = nvram_list_get(&list_system, opts.key);
+		if (!opts.val && !opts.system_mode) {
+			pr_dbg("getting key from user: %s\n", opts.key);
 			opts.val = nvram_list_get(&list_user, opts.key);
-			if (!opts.val) {
-				r = -ENOENT;
-				goto exit;
-			}
+		}
+		if (!opts.val) {
+			pr_dbg("key not found: %s\n", opts.key);
+			r = -ENOENT;
+			goto exit;
 		}
 		printf("%s\n", opts.val);
 		break;
 	case OP_SET:
-		pr_dbg("setting: %s=%s\n", opts.key, opts.val);
-		if (!opts.factory_mode && nvram_list_get(&list_factory, opts.key)) {
-			printf("key: %s: already in immutable factory nvram -- abort\n", opts.key);
-			r = -EACCES;
-			goto exit;
-		}
-		else {
-			const char* userval = nvram_list_get(opts.factory_mode ? &list_factory : &list_user, opts.key);
-			if (!userval || strcmp(userval, opts.val)) {
-				r = nvram_list_set(opts.factory_mode ? &list_factory : &list_user, opts.key, opts.val);
-				if (r) {
-					pr_err("failed setting to list [%d]: %s\n", -r, strerror(-r));
-					goto exit;
-				}
+		pr_dbg("setting %s: %s=%s\n", opts.system_mode ? "system" : "user", opts.key, opts.val);
+		const char* val = nvram_list_get(opts.system_mode ? &list_system : &list_user, opts.key);
+		if (!val || strcmp(val, opts.val)) {
+			r = nvram_list_set(opts.system_mode ? &list_system : &list_user, opts.key, opts.val);
+			if (r) {
+				pr_err("failed setting to list [%d]: %s\n", -r, strerror(-r));
+				goto exit;
+			}
 
-				r = nvram_commit(opts.factory_mode ? nvram_factory : nvram_user, opts.factory_mode ? &list_factory : &list_user);
-				if (r) {
-					goto exit;
-				}
+			r = nvram_commit(opts.system_mode ? nvram_system : nvram_user, opts.system_mode ? &list_system : &list_user);
+			if (r) {
+				goto exit;
 			}
 		}
 		break;
 	case OP_DEL:
-		pr_dbg("deleting: %s\n", opts.key);
-		if(nvram_list_remove(opts.factory_mode ? &list_factory : &list_user, opts.key)) {
-			r = nvram_commit(opts.factory_mode ? nvram_factory : nvram_user, opts.factory_mode ? &list_factory : &list_user);
+		pr_dbg("deleting %s: %s\n", opts.system_mode ? "system" : "user", opts.key);
+		if(nvram_list_remove(opts.system_mode ? &list_system : &list_user, opts.key)) {
+			r = nvram_commit(opts.system_mode ? nvram_system : nvram_user, opts.system_mode ? &list_system : &list_user);
 			if (r) {
 				goto exit;
 			}
@@ -250,9 +295,9 @@ int main(int argc, char** argv)
 
 exit:
 	release_lockfile(NVRAM_LOCKFILE, FDLOCK);
-	destroy_nvram_list(&list_factory);
+	destroy_nvram_list(&list_system);
 	destroy_nvram_list(&list_user);
-	nvram_close(&nvram_factory);
+	nvram_close(&nvram_system);
 	nvram_close(&nvram_user);
 	return -r;
 }
