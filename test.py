29219be81556db99e3f63caab4874344665e6059
Mikko Salomäki <ms@datarespons.se>
2020-04-03 15:54:44 +0200

    test: add --sys tests

--- test.py
+++ test.py
@@ -6,22 +6,19 @@ import os
 import subprocess
 from subprocess import CalledProcessError
 
-def nvram(env, arglist):
+def nvram(env, arglist, sys=False):
     args = ['./nvram']
+    if sys:
+        args.append('--sys')
     args.extend(arglist)
     r = subprocess.run(args, capture_output=True, text=True, env=env, check=True)
     return r.stdout
 
-def nvram_set(env, key, val):
-    nvram(env, ['set', key, val])
+'''
 
-def nvram_get(env, key):
-    return nvram(env, ['get', key]).rstrip()
+            USER mode
 
-def nvram_list(env):
-    stdout = nvram(env, ['list'])
-    return dict(pair.split("=") for pair in stdout.split())
-
+'''   
 class test_user_base(unittest.TestCase):
     def setUp(self):
         self.tmpdir = tempfile.TemporaryDirectory()
@@ -32,33 +29,57 @@ class test_user_base(unittest.TestCase):
                 'NVRAM_USER_A': f'{self.dir}/user_a',
                 'NVRAM_USER_B': f'{self.dir}/user_b',
             }
+        self.sys = False
     
     def tearDown(self):
         self.assertFalse(os.path.isfile(self.env['NVRAM_SYSTEM_A']))
         self.assertFalse(os.path.isfile(self.env['NVRAM_SYSTEM_B']))
         self.tmpdir.cleanup()
+        
+    def nvram_set(self, key, val):
+        nvram(self.env, ['set', key, val], sys=self.sys)
+        
+    def nvram_get(self, key):
+        return nvram(self.env, ['get', key], sys=self.sys).rstrip()
+    
+    def nvram_list(self):
+        stdout = nvram(self.env, ['list'], sys=self.sys)
+        return dict(pair.split("=") for pair in stdout.split())
+    
+    def nvram_delete(self, key):
+        nvram(self.env, ['delete', key], sys=self.sys)
 
 class test_user_set_get(test_user_base):
     def test_set_get(self):
-        nvram_set(self.env, 'var1', 'val1')
-        var1 = nvram_get(self.env, 'var1')
-        self.assertEqual(var1, 'val1')
-        
+        key = 'key1'
+        val = 'val1'
+        self.nvram_set(key, val)
+        self.assertEqual(val, self.nvram_get(key))
+     
     def test_set_get_multiple(self):
         attributes = {}
         for i in range(10):
             key = f'key{i}'
             val = f'val{i}'
             attributes[key] = val
-            nvram_set(self.env, key, val)
+            self.nvram_set(key, val)
         
         for key, value in attributes.items():
-            read = nvram_get(self.env, key)
+            read = self.nvram_get(key)
             self.assertEqual(read, value)
-            
-    def test_get_empty(self):
+        
+    def test_empty(self):
+        key = 'key1'
         with self.assertRaises(CalledProcessError):
-            nvram_get(self.env, 'key1')
+            self.nvram_get(key)
+            
+    def test_overwrite(self):
+        key = 'key1'
+        val1 = 'val1'
+        val2 = 'val2'
+        self.nvram_set(key, val1)
+        self.nvram_set(key, val2)
+        self.assertEqual(val2, self.nvram_get(key))
         
 class test_user_list(test_user_base):
     def test_list(self):
@@ -67,13 +88,106 @@ class test_user_list(test_user_base):
             key = f'key{i}'
             val = f'val{i}'
             attributes[key] = val
-            nvram_set(self.env, key, val)
-        d = nvram_list(self.env)
+            self.nvram_set(key, val)
+        d = self.nvram_list()
         self.assertEqual(d, attributes)
         
     def test_empty(self):
-        d = nvram_list(self.env)
+        d = self.nvram_list()
         self.assertEqual(0, len(d))
         
+class test_user_delete(test_user_base):
+    def test_delete(self):
+        key = 'key1'
+        val = 'val1'
+        self.nvram_set(key, val)
+        self.nvram_delete(key)
+        with self.assertRaises(CalledProcessError):
+            self.nvram_get(key)
+            
+    def test_empty(self):
+        key = 'key1'
+        self.nvram_delete(key)
+  
+'''
+
+            SYSTEM mode (--sys)
+
+'''
+class test_system_base(test_user_base):
+    def setUp(self):
+        super().setUp()
+        self.sys = True
+        self.env['NVRAM_SYSTEM_UNLOCK'] = '16440'
+    
+    def tearDown(self):
+        self.assertFalse(os.path.isfile(self.env['NVRAM_USER_A']))
+        self.assertFalse(os.path.isfile(self.env['NVRAM_USER_B']))
+        self.tmpdir.cleanup()
+
+## FIXME: Testa kan itne skriva UTAN _SYS
+## FIXME: User kan inte skriva MED _SYS
+## FIXME: test lock
+
+class test_system_set_get(test_system_base):
+    def test_set_get(self):
+        key = 'SYS_key1'
+        val = 'val1'
+        self.nvram_set(key, val)
+        self.assertEqual(val, self.nvram_get(key))
+     
+    def test_set_get_multiple(self):
+        attributes = {}
+        for i in range(10):
+            key = f'SYS_key{i}'
+            val = f'val{i}'
+            attributes[key] = val
+            self.nvram_set(key, val)
+        
+        for key, value in attributes.items():
+            read = self.nvram_get(key)
+            self.assertEqual(read, value)
+        
+    def test_empty(self):
+        key = 'SYS_key1'
+        with self.assertRaises(CalledProcessError):
+            self.nvram_get(key)
+            
+    def test_overwrite(self):
+        key = 'SYS_key1'
+        val1 = 'val1'
+        val2 = 'val2'
+        self.nvram_set(key, val1)
+        self.nvram_set(key, val2)
+        self.assertEqual(val2, self.nvram_get(key))
+        
+class test_system_list(test_system_base):
+    def test_list(self):
+        attributes = {}
+        for i in range(10):
+            key = f'SYS_key{i}'
+            val = f'val{i}'
+            attributes[key] = val
+            self.nvram_set(key, val)
+        d = self.nvram_list()
+        self.assertEqual(d, attributes)
+        
+    def test_empty(self):
+        d = self.nvram_list()
+        self.assertEqual(0, len(d))
+        
+class test_system_delete(test_system_base):
+    def test_delete(self):
+        key = 'SYS_key1'
+        val = 'val1'
+        self.nvram_set(key, val)
+        self.nvram_delete(key)
+        with self.assertRaises(CalledProcessError):
+            self.nvram_get(key)
+            
+    def test_empty(self):
+        key = 'SYS_key1'
+        self.nvram_delete(key)
+
 if __name__ == '__main__':
     unittest.main()
